6253f3408f970b9f5453e790d8fcb8f4ab54aa2e
Naman0901 <73291949+Naman0901@users.noreply.github.com>
2023-01-23 15:58:44 +0530

    Delete Todo Frontend directory

--- Todo Frontend/src/Route.js
+++
@@ -1,9 +0,0 @@
-import ToDos from "./components/ToDos.vue";
-import Addto from "./components/Addtodo.vue";
-import axios from 'axios';
-
-export default[
-    {path: '/', component: ToDos},
-    {path: '/add', component: Addto}
-]
-
